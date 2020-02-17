c1c58356fca1fe8357fcf568fde195c521d63140
krzychuuu132 <krzychu222u@wp.pl>
2020-02-17 23:04:08 +0100

    initial commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./js/main.js"
+  },
+
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss|sass$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.jpg|png|svg$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "QUIZ-APP",
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    }),
+
+    new CopyPlugin([{ from: "./public/img", to: "img" }])
+  ]
+};
