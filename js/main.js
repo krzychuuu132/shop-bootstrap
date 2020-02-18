a262c3f684f24ef5268f355fd5ed0c951ea8e2d3
krzychuuu132 <krzychu222u@wp.pl>
2020-02-18 03:32:30 +0100

    creating products/main/footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
 import "../sass/style.scss";
 
-const elo = "siemmaa";
+const products = [
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  },
+  {
+    title: "Pierścionek",
+    text: "Lorem ipsum dolor.",
+    btn_text: "New!",
+    price: "59,99$",
+    src: "./public/img/jewelerry.jpg"
+  }
+];
